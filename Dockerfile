2397bde19a5a74d83715a3749ad0a3fef5992434
yijun <yijunge@berkeley.edu>
2025-05-13 10:09:52 -0700

    query change

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.9-slim
-RUN pip install flask
+RUN pip install flask requests
 COPY main.py /app/main.py
 WORKDIR /app
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,16 +1,31 @@
+import logging
 import json
-from flask import Flask, request, jsonify
-import requests
 import base64
 
-PROMETHEUS_URL = "http://support-prometheus-server.monitoring.svc.cluster.local:9090"
+
+from flask import Flask, request, jsonify
+import requests
+
 
 
 app = Flask(__name__)
 
+logging.basicConfig(
+    level=logging.INFO,                     # Set log level (DEBUG, INFO, WARNING, etc.)
+    format='%(asctime)s %(levelname)s %(message)s',
+    handlers=[logging.StreamHandler()]      # Log to stdout
+)
+
+
+PROMETHEUS_URL = "http://support-prometheus-server.support.svc.cluster.local"
+
 def query_prometheus(query):
-    response = requests.get(f"{PROMETHEUS_URL}/api/v1/query", params={"query": query})
+    params = {
+        'query': query,
+    } 
+    response = requests.get(f"{PROMETHEUS_URL}/api/v1/query", params=params)
     result = response.json()
+    print(result)
     if result["status"] == "success" and result["data"]["result"]:
         return float(result["data"]["result"][0]["value"][1])
     return None
@@ -31,7 +46,7 @@ def parse_memory(mem_str):
 def mutate():
     """
     Processes of mutating the CPU and memory requests. 
-    1. The single user container in a pod is identified by filtering the image ending with 'user-image'.
+    1. The single user container in a pod is identified by filtering the image name. 
     2. The app querys the Prometheus API for max(95 percentile usage for all the pods in the same namespace).
     3. The app sets the recommended CPU and memory requests to max(95 percentile). 
     """
@@ -44,9 +59,9 @@ def mutate():
     pod_name = pod["metadata"]["name"]
     containers = pod["spec"].get("containers", [])
 
-    # Find the container that ends with 'user-image'
+    # Find the container using single user images 
     result = next(
-        ((i, c) for i, c in enumerate(containers) if c.get("image", "").endswith("user-image")),
+        ((i, c) for i, c in enumerate(containers) if "user-images" in c.get("image", "")),
         (None, None)
     )
     target_index, target_container = result
@@ -71,48 +86,61 @@ def mutate():
         mem_request = parse_memory(mem_request_str)
         mem_limit = parse_memory(mem_limit_str)
 
-        print(
+        app.logger.info(
             f"Pod: {pod_name}, Container: {name}, Image: {image}, "
             f"CPU Request: {cpu_request}m, Memory Request: {mem_request}Mi, "
             f"CPU Limit: {cpu_limit}m, Memory Limit: {mem_limit}Mi"
         )
 
         # Prometheus queries
-        # 95th percentile CPU usage over a 1h window
+        # 95th percentile CPU usage over a 15mins window
         cpu_query_95 = f'''
-            max(quantile_over_time(0.95, rate(container_cpu_usage_seconds_total{{namespace="{namespace}", container!="", container!="mongo", container!="postgres", pod=~"jupyter-.*", cloud_google_com_gke_nodepool=~"user-.*"}}[5m])[1h:]))
+            quantile(0.95, avg_over_time(avg by (pod) (rate(container_cpu_usage_seconds_total{{namespace="{namespace}", container!="", container!="mongo", container!="postgres", pod=~"jupyter-.*", cloud_google_com_gke_nodepool=~"user-.*"}}[5m]))[15m:]))
         '''
-        # 95th percentile memory usage over a 1h window
+        
+        # 95th percentile memory usage over a 15mins window
         mem_query_95 = f'''
-            max(quantile_over_time(0.95, container_memory_usage_bytes{{namespace="{namespace}", container!="", container!="mongo", container!="postgres", pod=~"jupyter-.*", cloud_google_com_gke_nodepool=~"user-.*"}}[1h:]))
+            quantile(0.95, avg_over_time(avg by (pod) (container_memory_usage_bytes{{namespace="{namespace}", container!="", container!="mongo", container!="postgres", pod=~"jupyter-.*", cloud_google_com_gke_nodepool=~"user-.*"}})[15m:]))
         '''
         try:
             cpu_95_percentile = query_prometheus(cpu_query_95)
             mem_95_percentile = query_prometheus(mem_query_95)
         except:
-            print(f"Prometheus query failed in the namespace {namespace}. ")
+            app.logger.error(f"Prometheus query failed in the namespace {namespace}. ")
             cpu_95_percentile = None
             mem_95_percentile = None
 
         # Convert to Kubernetes expected units
         if cpu_95_percentile is not None:
             # CPU request can not exceed the CPU limit
-            recommended_cpu_millicores = min(int(cpu_95_percentile * 1000), cpu_limit) 
+            prometheus_cpu = int(cpu_95_percentile * 1000)
+            app.logger.info(f"Prometheus 95 percentile CPU is {prometheus_cpu} m.")
+            if cpu_limit != 0:
+                recommended_cpu_millicores = min(prometheus_cpu, cpu_limit) 
+            else:
+                recommended_cpu_millicores = prometheus_cpu
         else:
             # If no prometheus data is found, the CPU request is not modified.
+            app.logger.info("No Prometheus data is available for modifiying CPU requests. The CPU request will remain unchanged. ")
             recommended_cpu_millicores = cpu_request
 
         if mem_95_percentile is not None:
             # Memory request can not exceed the memory limit. 
-            recommended_mem_mebibytes = min(int(mem_95_percentile / 1024 / 1024), mem_limit)
+            prometheus_mem = int(mem_95_percentile / 1024 / 1024)
+            app.logger.info(f"Prometheus 95 percentile memory is {prometheus_mem} Mi.")
+            if mem_limit != 0:
+                recommended_mem_mebibytes = min(prometheus_mem, mem_limit)
+            else:
+                recommended_mem_mebibytes = prometheus_mem
         else:
             # If no prometheus data is found, the memory request is not modified. 
+            app.logger.info("No Prometheus data is available for modifiying memory requests. The memory request will remain unchanged. ")
             recommended_mem_mebibytes = mem_request
         
-        print(f"Recommended CPU Request: {recommended_cpu_millicores}m")
-        print(f"Recommended Memory Request: {recommended_mem_mebibytes}Mi")
+        app.logger.info(f"Recommended CPU Request: {recommended_cpu_millicores}m")
+        app.logger.info(f"Recommended Memory Request: {recommended_mem_mebibytes}Mi")
     else:
-        print(f"Pod: {pod_name}, No container found with image ending in 'user-image'")
+        app.logger.warning(f"Pod: {pod_name}. No container found using single user images.")
 
 
     if target_index is not None:
